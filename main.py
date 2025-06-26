4892f7c4ec0e76ccc1f6b26e8c1f663a30aa5d30
Taras <beltengevr@gmail.com>
2025-06-26 23:10:54 +0300

    Add python file

---
+++ main.py
@@ -0,0 +1,17 @@
+class Parser:
+    def __init__(self):
+        self.id = 5
+
+    def teest(self):
+        value = 10
+        self.value(value)
+
+    def value(self, value):
+        print(str(value))
+
+
+
+
+text = Parser()
+
+print(text.teest())
